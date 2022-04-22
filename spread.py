a7ef9ed82c9303a0847a4dbcae0c7393f794ad14
lvucovix <62305720+lvucovix@users.noreply.github.com>
2022-04-21 23:12:28 -0300

    v1 funcionando

--- spread.py
+++ spread.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-from time import sleep
 import telebot
 
 #importa o bot do telegram
@@ -18,46 +17,58 @@ print(f'O preço do dólar é: {precoDolar}')
 #par dolar de cada moeda BINANCE
 binanceBase = 'USDT'
 
-for moeda in listaMoedas:
-  requisicaoStatusBinance = requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').status_code
-  requisicaoStatusMB = requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').status_code
-  requisicaoStatusNovaDax = requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').status_code
+c = 1
+while c <5:
+  print(f'{c}ª verificação')
+  #Binance X MB
+  print('Verificando Binance x MB') 
+  for moeda in listaMoedas:
+    requisicaoStatusBinance = requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').status_code
+    requisicaoStatusMB = requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').status_code
 
- #Binance X MB 
-  if (requisicaoStatusBinance == 200) and (requisicaoStatusMB == 200):
-    precoBinance = float(requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').json()['price'])
-    precoMB = (float(requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').json()['ticker']['last']))/precoDolar
-    porcentoBinancexMB = ((precoBinance/precoMB)-1)*100
-
-    if (porcentoBinancexMB >=0.02):
-      print(f'Compra {moeda} no MB por {precoMB} vende na Binance por {precoBinance}')
-    elif (porcentoBinancexMB <=-0.02):
-      print(f'Compra {moeda} na Binance por {precoBinance} vende no MB por {precoMB}')
+    if (requisicaoStatusBinance == 200) and (requisicaoStatusMB == 200):
+      precoBinance = float(requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').json()['price'])
+      precoMB = (float(requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').json()['ticker']['last']))/precoDolar
+      porcentoBinancexMB = ((precoBinance/precoMB)-1)*100
+      
+      if (porcentoBinancexMB >=28):
+        print(f'Compra {moeda} no MB por {precoMB} vende na Binance por {precoBinance}')
+        bot.send_message(-763471222, f'Compra {moeda} no MB por {precoMB} vende na Binance por {precoBinance}')
+      elif (porcentoBinancexMB <=-28):
+        print(f'Compra {moeda} na Binance por {precoBinance} vende no MB por {precoMB}')
+        bot.send_message(-763471222, f'Compra {moeda} na Binance por {precoBinance} vende no MB por {precoMB}')
 
   #MB X NOVADAX
-  elif (requisicaoStatusMB == 200) and (requisicaoStatusNovaDax==200):
-    precoMB = (float(requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').json()['ticker']['last']))/precoDolar 
-    precoNovaDax = (float(requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').json()['data']['bid']))/precoDolar
-    porcentoMBxNovaDax = ((precoMB/precoNovaDax)-1)*100
+  print('Verificando MB x NovaDax')
+  for moeda in listaMoedas:
+    requisicaoStatusMB = requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').status_code
+    requisicaoStatusNovaDax = requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').status_code
 
-    if (porcentoMBxNovaDax >=0.02):
-      print('NÃO TEM NA BINANCE')
-      print(f'Compra {moeda} na NovaDax por {precoNovaDax} vende no MB por {precoMB}')
-    elif (porcentoMBxNovaDax <=-0.02):
-      print('NÃO TEM NA BINANCE')
-      print(f'Compra {moeda} no MB por {precoMB} vende na NovaDax por {precoNovaDax}')
+    if (requisicaoStatusMB == 200) and (requisicaoStatusNovaDax == 200):
+      precoMB = (float(requests.get(f'https://www.mercadobitcoin.net/api/{moeda}/ticker').json()['ticker']['last']))/precoDolar
+      precoNovaDax = float(requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').json()['data']['bid'])/precoDolar
+      porcentoMBxNovaDax = ((precoMB/precoNovaDax)-1)*100
+
+      
+      if (porcentoMBxNovaDax >=28):
+        print(f'Compra {moeda} na NovaDax por {precoNovaDax} vende no MB por {precoMB}')
+      elif (porcentoMBxNovaDax <=-28):
+        print(f'Compra {moeda} no MB por {precoMB} vende na NovaDax por {precoNovaDax}')
+
   #Binance X NovaDax
-  elif (requisicaoStatusBinance == 200) and (requisicaoStatusNovaDax == 200):
-    precoBinance = float(requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').json()['price'])
-    precoNovaDax = (float(requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').json()['data']['bid']))/precoDolar
-    porcentoBinancexNovaDax = ((precoMB/precoNovaDax)-1)*100
-    if (porcentoBinancexNovaDax >=0.02):
-      print("BINANCE X NOVADAX")
-      print(f'Compra {moeda} na NovaDax por {precoNovaDax} vende na Binance por {precoBinance}')
-    elif (porcentoBinancexNovaDax <=-0.02):
-      print("BINANCE X NOVADAX")
-      print(f'Compra {moeda} na Binance por {precoBinance} vende na NovaDax por {precoNovaDax}')
+  print('Verificando Binance x NovaDax')
+  for moeda in listaMoedas:
+    requisicaoStatusBinance = requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').status_code
+    requisicaoStatusNovaDax = requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').status_code
 
-    
-  else:  
-    print(f'Sem par compatível de {moeda}')
+    if (requisicaoStatusBinance == 200) and (requisicaoStatusNovaDax == 200):
+      precoBinance = float(requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={moeda+binanceBase}').json()['price'])
+      precoNovaDax = float(requests.get(f'https://api.novadax.com/v1/market/ticker?symbol={moeda}_BRL').json()['data']['bid'])/precoDolar
+      porcentoBinancexMB = ((precoBinance/precoNovaDax)-1)*100
+
+      
+      if (porcentoBinancexMB >=28):
+        print(f'Compra {moeda} no MB por {precoNovaDax} vende na Binance por {precoBinance}')
+      elif (porcentoBinancexMB <=-28):
+        print(f'Compra {moeda} na Binance por {precoBinance} vende no MB por {precoNovaDax}')
+  c+=1
